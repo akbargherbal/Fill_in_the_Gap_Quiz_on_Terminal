9bec2668f457c7dd337685babdee463440980fe1
Akbar Gherbal <ghurbal.akbar@gmail.com>
2022-11-21 09:57:04 +0300

    Git pushed from inside python script @ 2022-11-21 09:57:03.718618

--- play.py
+++ play.py
@@ -15,10 +15,14 @@ for i in range (huge_number):
             print(Fore.GREEN, '''
 Do you want to play again? Enter (yes/no )or (y/n):''')
             play_again = input('Enter your choice: ')
-            if play_again.lower().strip()[0] != 'y':
-                print(Fore.LIGHTRED_EX, 'Thank you for playing! Goodbye!')
+            while play_again.lower().strip()[0] not in ['y', 'n']:
+                print(Fore.LIGHTRED_EX, 'Did not understand your choice, please enter y or n:')
+                play_again = input('Enter your choice: ')
+            if play_again.lower().strip()[0] == 'n':
+                print(Fore.LIGHTRED_EX, 'Goodbye!')
                 break
-            exec(code)
+            else:
+                exec(code)
         else:
             exec(code)
             
